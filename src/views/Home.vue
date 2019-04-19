e9ecbd89e9b8ff67cbe5e653cee59eae0ba8d7c1
Christopher Maree <christopher.maree@gmail.com>
2019-04-19 21:10:16 +0200

    Added basic home page

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-container">
+    Create a new automatically rebalanced portfolio!
+<br>
+    Some awesome text and images to show what we are all about!
+<br>
+    explain how the user needs a wallet ect. and then drive to the button below.
+    <br>
+    <md-button class="md-raised" @click="handelLogin">Begin</md-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+import router from "@/router";
+
+export default {
+  name: "home",
+  components: {},
+  methods: {
+    handelLogin() {
+      router.push({ name: "create" });
+    }
+  }
+};
+</script>
