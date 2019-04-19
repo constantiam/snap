d6dc2f3a1e84e35dd06ddf6718eadb9968597ac1
Veronica <veronica.21.coutts@gmail.com>
2019-04-19 21:44:48 +0200

    fund start

---
+++ contracts/Fund.sol
@@ -0,0 +1,57 @@
+pragma solidity 0.4.18;
+
+contract Fund {
+    address private _owner;
+    address[] private _tokens;
+    uint8[] private _distribution;
+
+    modifier isOwner() {
+        require(msg.sender() == _owner, "Access Denied");
+    }
+
+    constructor(address[] _tokenAddresses, uint8[] _percentages) 
+        public
+        payable
+    {
+        _owner = msg.sender();
+        _tokens = _tokenAddresses;
+        _distribution = _percentages;
+    }
+
+    function addTokens(address[] _tokenAddresses, uint8[] _percentages) 
+        public
+        isOwner()
+    {
+        _tokens = _tokenAddresses;
+        _distribution = _percentages;
+        //TODO for loop approving the rebalancer to spend tokens
+    }
+
+    function rebalance()
+        public
+        isOwner()
+    {
+        //TODO
+    }
+
+    function manualRebalance()
+        public
+        isOwner()
+    {
+        //TODO
+    }
+
+    function killFund()
+        public
+        isOwner()
+    {
+        //TODO
+    }
+
+    function() 
+        public 
+        payable 
+    { 
+        
+    }
+}
