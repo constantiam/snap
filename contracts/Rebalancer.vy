f1f387b00501ae7aa82a507d16db75fcb039efd2
Christopher Maree <christopher.maree@gmail.com>
2019-04-20 11:59:58 +0200

    Added another token

--- contracts/Rebalancer.vy
+++ contracts/Rebalancer.vy
@@ -1,8 +1,5 @@
 #each component of the fund has a token address and the desirned weighting
 
-
-
-
 contract Factory():
   def getExchange(token_addr: address) -> address: constant
 
@@ -26,30 +23,6 @@ def __init__(_uniswapFactory: address):
   self.owner = msg.sender
   self.uniswapFactory = _uniswapFactory
 
-@payable
-@public
-def rebalanceFund(_tokenAddress: address[100], _weightings: uint256[100], _numberOfTokens: int128) -> uint256(wei):
-  totalFundValue: uint256
-  for i in range(0,100):
-    if i == _numberOfTokens:
-      break
-    self.token = _tokenAddress[i]
-    exchange_addr: address = self.uniswapFactory.getExchange(_tokenAddress[i])
-    self.exchange = exchange_addr
-    exchangeTokenBalance: uint256 = self.token.balanceOf(exchange_addr)
-    exchangeTokenBalanceDecimal: uint256(wei) = as_wei_value(exchangeTokenBalance,'wei')
-    exchangeEtherBalance: uint256(wei) = exchange_addr.balance
-    # exchangeEtherBalanceDecimal: decimal = convert(exchangeEtherBalance, decimal)
-    currentTokenPrice: uint256(wei) = as_wei_value(exchangeTokenBalanceDecimal / exchangeEtherBalance,'wei')
-  
-    totalFundValue += self.token.balanceOf(msg.sender)
-  
-  return 10
-  # return self.exchange.ethToTokenTransferOutput(_tokens, block.timestamp + 10, msg.sender, value = msg.value)
-  
-  # self.token.approve(exchange_addr, _tokens)
-  # return self.exchange.tokenToEthSwapInput(_tokens, 1, block.timestamp + 10)
-  # return True
 
 @public
 def getTokenPrice(_tokenAddress: address) -> decimal:
@@ -74,16 +47,44 @@ def getTokenPriceUint(_tokenAddress: address) -> uint256:
   return (exchangeEtherBalanceUint*10**18) / exchangeTokenBalance
   
 @public
-def getFundValue(_tokenAddress: address[1], _numberOfTokens: int128) -> uint256:
-  totalFundValue: uint256
+def getWalletValue(_tokenAddress: address[100], _numberOfTokens: int128) -> uint256:
+  totalWalletValue: uint256
   for i in range(0,100):
     if i == _numberOfTokens:
       break
     self.token = _tokenAddress[i]
     tokenPrice: uint256 = self.getTokenPriceUint(_tokenAddress[i])
     numberOfTokens: uint256 = self.token.balanceOf(msg.sender)
-    totalFundValue += tokenPrice * numberOfTokens
-  return totalFundValue / 10**18
+    totalWalletValue += tokenPrice * numberOfTokens
+  return totalWalletValue / 10**18
+
+
+
+@payable
+@public
+def rebalanceFund(_tokenAddress: address[100], _weightings: uint256[100], _numberOfTokens: int128) -> uint256(wei):
+  totalFundValue: uint256 = self.getWalletValue(_tokenAddress, _numberOfTokens)
+  for i in range(0,100):
+    if i == _numberOfTokens:
+      break
+    self.token = _tokenAddress[i]
+    exchange_addr: address = self.uniswapFactory.getExchange(_tokenAddress[i])
+    self.exchange = exchange_addr
+    exchangeTokenBalance: uint256 = self.token.balanceOf(exchange_addr)
+    exchangeTokenBalanceDecimal: uint256(wei) = as_wei_value(exchangeTokenBalance,'wei')
+    exchangeEtherBalance: uint256(wei) = exchange_addr.balance
+    # exchangeEtherBalanceDecimal: decimal = convert(exchangeEtherBalance, decimal)
+    currentTokenPrice: uint256(wei) = as_wei_value(exchangeTokenBalanceDecimal / exchangeEtherBalance,'wei')
+  
+    totalFundValue += self.token.balanceOf(msg.sender)
+  
+  return 10
+  # return self.exchange.ethToTokenTransferOutput(_tokens, block.timestamp + 10, msg.sender, value = msg.value)
+  
+  # self.token.approve(exchange_addr, _tokens)
+  # return self.exchange.tokenToEthSwapInput(_tokens, 1, block.timestamp + 10)
+  # return True
+
 
 @public
 @payable
--- test/constantium_fund.test.js
+++ test/constantium_fund.test.js
@@ -33,10 +33,13 @@ contract("Rebalancer", (accounts) => {
     const monthDuration = 2628000000
     const deadline = 1742680400 //some point in the future
 
-    //for this given reserve ratio the price/eth will be 100 dai
+    //for this given reserve ratio the price of 1 dai in eth will be 0.01 eth/dai 
+    // giving the value of 100 usd per eth. For mkr a price of  5 eth/mkr is set
     const ethReserve = web3.utils.toWei("1000", 'ether')
     const daiReserve = web3.utils.toWei("100000", 'ether')
+    const mkrReserve = web3.utils.toWei("200", 'ether')
     const daiSupply = web3.utils.toWei("1000000", 'ether')
+
 
     before(async function () {
         //start by creating a template contract
@@ -52,7 +55,7 @@ contract("Rebalancer", (accounts) => {
             from: uniSwapOwner
         })
         //next, we need a token for our uniswap exchange. make a fake dai contract
-        erc20Token = await ERC20Token.new(
+        daiTokenContract = await ERC20Token.new(
             web3.utils.toHex("Dai Mock"),
             web3.utils.toHex("DAI"),
             18,
@@ -60,45 +63,89 @@ contract("Rebalancer", (accounts) => {
                 from: tokenOwner
             })
         //we can now make an exchange for the fake dai contract from the factory
-        await uniswapFactory.createExchange(erc20Token.address, {
+        await uniswapFactory.createExchange(daiTokenContract.address, {
             from: tokenOwner
         })
         //grab the exchange address and make an instance of it so we can add liquidity and trade against it
-        let exchangeAddress = await uniswapFactory.getExchange(erc20Token.address)
-        let uniswapExchange = await UniSwapExchange.at(exchangeAddress)
+        let mkrExchangeAddress = await uniswapFactory.getExchange(daiTokenContract.address)
+        let uniswapDaiExchange = await UniSwapExchange.at(mkrExchangeAddress)
 
         //approve before we add liquidity
-        await erc20Token.approve(exchangeAddress,
+        await daiTokenContract.approve(mkrExchangeAddress,
             daiSupply, {
                 from: tokenOwner
             })
 
-        let amountAproved = await erc20Token.allowance(tokenOwner, exchangeAddress)
+        let amountAproved = await daiTokenContract.allowance(tokenOwner, mkrExchangeAddress)
         console.log("Aproved")
         console.log(amountAproved.toString())
 
         //add the liquidity. 100000 dai and 1000 ether gives a starting price of 100 usd/eth
         //the min_liquidity param does not matter for initial liquidity addition
         //set the deadline way into the future. 
-        await uniswapExchange.addLiquidity(0,
+        await uniswapDaiExchange.addLiquidity(0,
             daiReserve,
             deadline, {
                 from: tokenOwner,
                 value: ethReserve
             })
 
-        let ethBalance = await web3.eth.getBalance(uniswapExchange.address)
-        console.log("ETH BAL")
+        let ethBalance = await web3.eth.getBalance(uniswapDaiExchange.address)
+        console.log("Dai exchange ETH balance")
         console.log(ethBalance.toString())
 
-        let tokenBalance = await erc20Token.balanceOf(uniswapExchange.address)
-        console.log("dai ba")
+        let tokenBalance = await daiTokenContract.balanceOf(uniswapDaiExchange.address)
+        console.log("Dai exchange DAI balance")
         console.log(tokenBalance.toString())
 
         let currentEthPrice = tokenBalance / ethBalance
 
-        console.log("Eth price in dai")
+        console.log("Dai exchange ETH balance price per eth in dai")
         console.log(currentEthPrice.toString())
+
+        //CREATE ANOTHER TOKEN FOR TESTING.
+        mkrTokenContract = await ERC20Token.new(
+            web3.utils.toHex("Mkr Mock"),
+            web3.utils.toHex("MKR"),
+            18,
+            daiSupply, {
+                from: tokenOwner
+            })
+        //we can now make an exchange for the fake dai contract from the factory
+        await uniswapFactory.createExchange(mkrTokenContract.address, {
+            from: tokenOwner
+        })
+        //grab the exchange address and make an instance of it so we can add liquidity and trade against it
+        mkrExchangeAddress = await uniswapFactory.getExchange(mkrTokenContract.address)
+        uniswapMkrExchange = await UniSwapExchange.at(mkrExchangeAddress)
+
+        //approve before we add liquidity
+        await mkrTokenContract.approve(mkrExchangeAddress,
+            daiSupply, {
+                from: tokenOwner
+            })
+
+        await uniswapMkrExchange.addLiquidity(0,
+            mkrReserve,
+            deadline, {
+                from: tokenOwner,
+                value: ethReserve
+            })
+
+        ethBalance = await web3.eth.getBalance(uniswapMkrExchange.address)
+        console.log("Mkr exchange ETH balance")
+        console.log(ethBalance.toString())
+
+        tokenBalance = await mkrTokenContract.balanceOf(uniswapMkrExchange.address)
+        console.log("Mkr exchange Mkr balance")
+        console.log(tokenBalance.toString())
+
+        currentEthPrice = tokenBalance / ethBalance
+
+        console.log("Mkr exchange ETH balance price per eth in Mkr")
+        console.log(currentEthPrice.toString())
+
+
 
         rebalancer = await Rebalancer.new(uniswapFactory.address, {
             from: owner
@@ -114,7 +161,7 @@ contract("Rebalancer", (accounts) => {
         // console.log("ETH BAL")
         // console.log(ethBalance.toString())
 
-        // tokenBalance = await erc20Token.balanceOf(uniswapExchange.address)
+        // tokenBalance = await daiTokenContract.balanceOf(uniswapExchange.address)
         // console.log("dai ba")
         // console.log(tokenBalance.toString())
 
@@ -126,29 +173,29 @@ contract("Rebalancer", (accounts) => {
 
 
         // console.log("GETTING PRICE")
-        // let contractPrice = await rebalancer.getTokenPriceUint.call(erc20Token.address)
+        // let contractPrice = await rebalancer.getTokenPriceUint.call(daiTokenContract.address)
         // console.log(contractPrice.toString())
 
         // console.log("getting value")
-        // let array = erc20Token.address
-        // let fundValue = await rebalancer.getFundValue.call([erc20Token.address], 1, {
+        // let array = daiTokenContract.address
+        // let fundValue = await rebalancer.getFundValue.call([daiTokenContract.address], 1, {
         //     from: tokenOwner
         // })
         // console.log(fundValue.toString())
 
 
-        // tokenBalance = await erc20Token.balanceOf(rebalancer.address)
+        // tokenBalance = await daiTokenContract.balanceOf(rebalancer.address)
         // console.log("~dai ba")
         // console.log(tokenBalance.toString())
-        // // await erc20Token.transfer(rebalancer.address, web3.utils.toWei("1", 'ether'), {
+        // // await daiTokenContract.transfer(rebalancer.address, web3.utils.toWei("1", 'ether'), {
         // //     from: tokenOwner
         // // })
 
-        // tokenBalance = await erc20Token.balanceOf(randomAddress)
+        // tokenBalance = await daiTokenContract.balanceOf(randomAddress)
         // console.log("balanceBefore")
         // console.log(tokenBalance.toString())
 
-        // await rebalancer.rebalanceFund(erc20Token.address, web3.utils.toWei("1", 'ether'), {
+        // await rebalancer.rebalanceFund(daiTokenContract.address, web3.utils.toWei("1", 'ether'), {
         //     from: randomAddress,
         //     value: web3.utils.toWei("1", 'ether')
         // })
@@ -156,7 +203,7 @@ contract("Rebalancer", (accounts) => {
         // // console.log("VA")
         // // console.log(valueWeiToSend.toString())
 
-        // tokenBalance = await erc20Token.balanceOf(randomAddress)
+        // tokenBalance = await daiTokenContract.balanceOf(randomAddress)
         // console.log("balance after")
         // console.log(tokenBalance.toString())
 
@@ -179,18 +226,20 @@ contract("Rebalancer", (accounts) => {
         })
         context("Evaluation Functions", function () {
             it("should correctly calculate the eth price based on balances of uniswap exchange", async () => {
-                let contractPrice = await rebalancer.getTokenPriceUint.call(erc20Token.address)
+                let contractPrice = await rebalancer.getTokenPriceUint.call(daiTokenContract.address)
                 //starting values of uniswap should make the starting price 1/100th of an eth = 10000000000000000 wei. 
                 //could not get .should.be.bignumber.equal to work so will hard code it for now....
                 assert.equal(contractPrice.toString(), '10000000000000000', "did not correctly calculate price");
             })
             it("should correctly calculate the value of a fund of tokens", async () => {
                 //send the randomAddress 100 tokens of dai, values at 1/100 eth per dai. 
-                await erc20Token.transfer(randomAddress, web3.utils.toWei("100", 'ether'), {
+                await daiTokenContract.transfer(randomAddress, web3.utils.toWei("100", 'ether'), {
                     from: tokenOwner
                 })
+                tokenArray = Array(100).fill(null).map((u, i) => '0x0000000000000000000000000000000000000000')
+                tokenArray[0] = daiTokenContract.address
                 //the balance of this position should be 1 eth @ 1/100dai per eth
-                let walletValue = await rebalancer.getFundValue.call([erc20Token.address], 1, {
+                let walletValue = await rebalancer.getWalletValue.call(tokenArray, 1, {
                     from: randomAddress
                 })
                 assert.equal(walletValue.toString(), '1000000000000000000', "did not correctly calculate wallet value");
